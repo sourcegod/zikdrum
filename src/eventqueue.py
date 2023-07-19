93bce00a0f82a1bf691c9ffba87bfc8456de2c8f
Coolbrother <coolbrother@sourcegod.com>
2023-07-19 17:29:03 +0200

    Updating: (push_event, pop_event functions, in EventQueue class)

--- src/eventqueue.py
+++ src/eventqueue.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
 """
     File: evenqueue.py:
-    Singleton object for transmitting midi events between objects
+    Implement a Singleton object and a ring buffer for transmitting midi events between objects
+    Inspired from Hydrogen Drumkit v0.9
     Date: Wed, 19/07/2023
     Author: Coolbrother
 """
+
+### Enum EventType
+EVENT_NONE =0
+EVENT_STATE =1
+EVENT_MIDI_ACTIVITY =2
+EVENT_NOTEON =3
+EVENT_ERROR =4
+EVENT_TEMPO_CHANGED =5
+EVENT_PROGRESS =6
+
+#-----------------------------------------
+
+class EventMessage(object):
+    type = EVENT_NONE
+    value =0
+
+#========================================
 
 class EventQueue(object):
     """ 
@@ -29,7 +47,32 @@ class EventQueue(object):
             print("Error: this Klass is a singleton klass.")
             return
         
+        self._max_ev =1024
+        self._ev_buffer = [EventMessage] * self._max_ev
+        self._read_index =0
+        self._write_index =0
+
     #-----------------------------------------
+
+    def push_event(self, type, value):
+        self._write_index = self._write_index + 1 % self._max_ev
+        ev = EventMessage()
+        ev.type = type
+        ev.value = value
+        self._ev_buffer[self._write_index] = ev
+
+    #-----------------------------------------
+
+    def pop_event(self):
+        if self._read_index == self._write_index:
+            ev = EventMessage()
+            return ev
+        
+        self._read_index = self._read_index + 1 % self._max_ev
+        return self._ev_buffer[self._read_index]
+
+    #-----------------------------------------
+
 
 #========================================
 
